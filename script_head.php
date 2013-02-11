2b684e0e1477faee5d9f9b8d092341570fbc4bde
Bob Stevenson <rstevenson555@comcast.net>
2013-02-11 16:41:52 -0600

    rearrange scripts

--- script_head.php
+++ script_head.php
@@ -1,16 +1,19 @@
-<!--<?php
-echo "<script src=\"scripts/opentag-48148-137748_old.js\" async defer ></script>"
-?>-->
-<script type="text/javascript">
+<?php
 
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-12682212-1']);
-          _gaq.push(['_trackPageview']);
+echo "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></script>\n";
+echo "<script type=\"text/javascript\">\n";
+echo "\n";
+echo "          var _gaq = _gaq || [];\n";
+echo "          _gaq.push(['_setAccount', 'UA-12682212-1']);\n";
+echo "          _gaq.push(['_trackPageview']);\n";
+echo "\n";
+echo "          (function() {\n";
+echo "            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;\n";
+echo "            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';\n";
+echo "            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);\n";
+echo "          })();\n";
+echo "\n";
+echo "        </script>\n";
+echo "<script src=\"http://www.blackhorsecarriers.com/scripts/home_animation.js\"></script>\n";
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
-
-        </script>
+?>
